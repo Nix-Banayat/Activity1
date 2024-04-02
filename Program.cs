051979b072d93cabded504ad11031feae05a4d12
Nix-Banayat <164652107+Nix-Banayat@users.noreply.github.com>
2024-04-02 19:55:46 +0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Activity1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string username = "Admin";
+            string password = "Admin1234!";
+            int attempts = 0;
+            int maxAttempts = 3;
+
+            while (attempts < maxAttempts)
+            {
+                Console.Write("Enter your username: \n");
+                string enterUsername = Console.ReadLine();
+
+                Console.Write("Enter your password: \n");
+                string enterPassword = Console.ReadLine();
+
+                if (enterUsername == username && enterPassword == password)
+                {
+                    Console.WriteLine("Login Successful");
+                    break;
+                }
+                else
+                {
+                    attempts++;
+                    Console.WriteLine("Incorrect Credentials. Try Again!\n(Your Attempts: " + attempts + " out of 3)\n");
+                }
+            }
+            if (attempts == maxAttempts)
+            {
+                Console.WriteLine("Sorry, Login Failed.");
+                Console.WriteLine("Maximum Attempts have been reached.");
+            }
+        }
+    }
+}
